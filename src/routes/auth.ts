28e7b1be49ac61431fe719c1fe8426399be6a298
Swackles <Swackles@users.noreply.github.com>
2021-01-19 11:32:02 +0200

    misleading error codes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -8,13 +8,13 @@ const router = express.Router();
 
 router.post('/register', async (req: Request, res: Response) => {
   const { email, password, name } = req.body;
-  if (!/\S+@\S+\.\S+/.test(email)) res.status(401).send(ErrorJSON(ErrorTypes.AUTH_INVALID_EMAIL))
+  if (!/\S+@\S+\.\S+/.test(email)) res.status(400).send(ErrorJSON(ErrorTypes.AUTH_INVALID_EMAIL))
 
   await client.connect();
 
   // checks if user already registered with email 
   let result = await client.query('SELECT * FROM users WHERE email=$1', [email])
-  if (result.rows.length != 0) return res.status(401).send(ErrorJSON(ErrorTypes.USER_EXISTS))
+  if (result.rows.length != 0) return res.status(400).send(ErrorJSON(ErrorTypes.USER_EXISTS))
 
   const token = crypto.randomBytes(50).toString('hex')
   console.log(token)
