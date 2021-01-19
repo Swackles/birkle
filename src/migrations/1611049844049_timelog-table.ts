f55c741c00cc59d9ba6506db85f612237c6f5ca2
Swackles <Swackles@users.noreply.github.com>
2021-01-19 12:55:47 +0200

    changed timelogs table and column naming

--- src/migrations/1611049844049_timelog-table.ts
+++ src/migrations/1611049844049_timelog-table.ts
@@ -5,20 +5,20 @@ import { MigrationBuilder } from "node-pg-migrate";
 exports.shorthands = undefined;
 
 exports.up = (pgm: MigrationBuilder) => {
-  pgm.createTable('timeLogs', {
+  pgm.createTable('time_logs', {
     id: 'id',
-    userId: {
+    user_id: {
       type: 'integer',
       notNull: true,
       references: 'users',
       onDelete: 'CASCADE'
     },
     description: { type: 'varchar(200)', notNull: true },
-    startTime: { type: 'timestamp', notNull: true },
-    endTime: { type: 'timestamp' },
+    start_time: { type: 'timestamp', notNull: true },
+    end_time: { type: 'timestamp' },
   })
 };
 
 exports.down = (pgm: MigrationBuilder) => {
-  pgm.dropTable('timeLogs')
+  pgm.dropTable('time_logs')
 };
